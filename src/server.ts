9cf2d127beae8525bb9467d70e74d905414e6de4
Lucas-Marques-Leao <lucasmarquesleao@outlook.com>
2022-06-29 12:29:33 -0300

    feat: dotenv app port

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import app from './app';
 import 'dotenv/config';
 import './database';
 
-app.listen(3333, () => {
+app.listen(Number(process.env.APP_PORT), () => {
     console.log('App Started')
 })
